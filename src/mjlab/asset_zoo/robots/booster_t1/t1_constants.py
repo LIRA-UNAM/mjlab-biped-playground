7c33d105b510b48a5038e3ce6eb294612b32de3c
Miguel Garcia <garcia.miguel.onate@gmail.com>
2026-04-08 15:41:35 -0600

    added booster to the asset zoo

--- src/mjlab/asset_zoo/robots/booster_t1/t1_constants.py
+++ src/mjlab/asset_zoo/robots/booster_t1/t1_constants.py
@@ -3,218 +3,145 @@
 from pathlib import Path
 
 import mujoco
-
-from mjlab import MJLAB_SRC_PATH
 from mjlab.actuator import BuiltinPositionActuatorCfg
 from mjlab.entity import EntityArticulationInfoCfg, EntityCfg
-from mjlab.utils.actuator import (
-  ElectricActuator,
-  reflected_inertia_from_two_stage_planetary,
-)
-from mjlab.utils.os import update_assets
+from mjlab.utils.actuator import ElectricActuator, reflected_inertia
 from mjlab.utils.spec_config import CollisionCfg
 
 ##
 # MJCF and assets.
 ##
 
-G1_XML: Path = (
-  MJLAB_SRC_PATH / "asset_zoo" / "robots" / "booster_t1" / "xml" / "t1.xml"
-)
-assert G1_XML.exists()
-
-
-def get_assets(meshdir: str) -> dict[str, bytes]:
-  assets: dict[str, bytes] = {}
-  update_assets(assets, G1_XML.parent / "assets", meshdir)
-  return assets
+T1_XML: Path = Path(__file__).parent / "xmls" / "t1.xml"
+assert T1_XML.exists()
 
 
 def get_spec() -> mujoco.MjSpec:
-  spec = mujoco.MjSpec.from_file(str(G1_XML))
-  spec.assets = get_assets(spec.meshdir)
-  return spec
+  return mujoco.MjSpec.from_file(str(T1_XML))
 
 
 ##
 # Actuator config.
+# Reference: https://booster.feishu.cn/wiki/JGZAwk8CUi5m6nklgxMcp2KlnVe
 ##
 
-# Motor specs (from Unitree).
-ROTOR_INERTIAS_5020 = (
-  0.139e-4,
-  0.017e-4,
-  0.169e-4,
-)
-GEARS_5020 = (
-  1,
-  1 + (46 / 18),
-  1 + (56 / 16),
-)
-ARMATURE_5020 = reflected_inertia_from_two_stage_planetary(
-  ROTOR_INERTIAS_5020, GEARS_5020
+_rpm = lambda r: r * 2 * 3.14159265 / 60  # noqa: E731
+
+NECK_ACTUATOR = ElectricActuator(
+  reflected_inertia=reflected_inertia(18e-6, 10),
+  velocity_limit=_rpm(400),
+  effort_limit=7.0,
 )
 
-ROTOR_INERTIAS_7520_14 = (
-  0.489e-4,
-  0.098e-4,
-  0.533e-4,
-)
-GEARS_7520_14 = (
-  1,
-  4.5,
-  1 + (48 / 22),
-)
-ARMATURE_7520_14 = reflected_inertia_from_two_stage_planetary(
-  ROTOR_INERTIAS_7520_14, GEARS_7520_14
+ARM_ACTUATOR = ElectricActuator(
+  reflected_inertia=reflected_inertia(21.8e-6, 36),
+  velocity_limit=_rpm(89),
+  effort_limit=36.0,
 )
 
-ROTOR_INERTIAS_7520_22 = (
-  0.489e-4,
-  0.109e-4,
-  0.738e-4,
-)
-GEARS_7520_22 = (
-  1,
-  4.5,
-  5,
-)
-ARMATURE_7520_22 = reflected_inertia_from_two_stage_planetary(
-  ROTOR_INERTIAS_7520_22, GEARS_7520_22
+WAIST_HIP_ROLL_YAW_ACTUATOR = ElectricActuator(
+  reflected_inertia=reflected_inertia(76.5e-6, 25),
+  velocity_limit=_rpm(70),
+  effort_limit=40.0,
 )
 
-ROTOR_INERTIAS_4010 = (
-  0.068e-4,
-  0.0,
-  0.0,
-)
-GEARS_4010 = (
-  1,
-  5,
-  5,
-)
-ARMATURE_4010 = reflected_inertia_from_two_stage_planetary(
-  ROTOR_INERTIAS_4010, GEARS_4010
+HIP_PITCH_ACTUATOR = ElectricActuator(
+  reflected_inertia=reflected_inertia(161.7e-6, 18),
+  velocity_limit=_rpm(157),
+  effort_limit=55.0,
 )
 
-ACTUATOR_5020 = ElectricActuator(
-  reflected_inertia=ARMATURE_5020,
-  velocity_limit=37.0,
-  effort_limit=25.0,
-)
-ACTUATOR_7520_14 = ElectricActuator(
-  reflected_inertia=ARMATURE_7520_14,
-  velocity_limit=32.0,
-  effort_limit=88.0,
-)
-ACTUATOR_7520_22 = ElectricActuator(
-  reflected_inertia=ARMATURE_7520_22,
-  velocity_limit=20.0,
-  effort_limit=139.0,
-)
-ACTUATOR_4010 = ElectricActuator(
-  reflected_inertia=ARMATURE_4010,
-  velocity_limit=22.0,
-  effort_limit=5.0,
+KNEE_ACTUATOR = ElectricActuator(
+  reflected_inertia=reflected_inertia(196.3e-6, 18),
+  velocity_limit=_rpm(140),
+  effort_limit=65.0,
 )
 
-NATURAL_FREQ = 10 * 2.0 * 3.1415926535  # 10Hz
+ANKLE_ACTUATOR = ElectricActuator(
+  reflected_inertia=reflected_inertia(26.2e-6, 36),
+  velocity_limit=_rpm(117),
+  effort_limit=50.0,
+)
+
+NATURAL_FREQ = 5.0 * 2.0 * 3.14159265  # 5 Hz
 DAMPING_RATIO = 2.0
 
-STIFFNESS_5020 = ARMATURE_5020 * NATURAL_FREQ**2
-STIFFNESS_7520_14 = ARMATURE_7520_14 * NATURAL_FREQ**2
-STIFFNESS_7520_22 = ARMATURE_7520_22 * NATURAL_FREQ**2
-STIFFNESS_4010 = ARMATURE_4010 * NATURAL_FREQ**2
 
-DAMPING_5020 = 2.0 * DAMPING_RATIO * ARMATURE_5020 * NATURAL_FREQ
-DAMPING_7520_14 = 2.0 * DAMPING_RATIO * ARMATURE_7520_14 * NATURAL_FREQ
-DAMPING_7520_22 = 2.0 * DAMPING_RATIO * ARMATURE_7520_22 * NATURAL_FREQ
-DAMPING_4010 = 2.0 * DAMPING_RATIO * ARMATURE_4010 * NATURAL_FREQ
+def _kp(act: ElectricActuator) -> float:
+  return act.reflected_inertia * NATURAL_FREQ**2
 
-G1_ACTUATOR_5020 = BuiltinPositionActuatorCfg(
+
+def _kv(act: ElectricActuator) -> float:
+  return 2.0 * DAMPING_RATIO * act.reflected_inertia * NATURAL_FREQ
+
+
+T1_ACTUATOR_NECK = BuiltinPositionActuatorCfg(
+  joint_names_expr=("AAHead_yaw", "Head_pitch"),
+  stiffness=_kp(NECK_ACTUATOR),
+  damping=_kv(NECK_ACTUATOR),
+  effort_limit=NECK_ACTUATOR.effort_limit,
+  armature=NECK_ACTUATOR.reflected_inertia,
+)
+
+T1_ACTUATOR_ARM = BuiltinPositionActuatorCfg(
   joint_names_expr=(
-    ".*_elbow_joint",
-    ".*_shoulder_pitch_joint",
-    ".*_shoulder_roll_joint",
-    ".*_shoulder_yaw_joint",
-    ".*_wrist_roll_joint",
+    ".*_Shoulder_Pitch",
+    ".*_Shoulder_Roll",
+    ".*_Elbow_Pitch",
+    ".*_Elbow_Yaw",
   ),
-  stiffness=STIFFNESS_5020,
-  damping=DAMPING_5020,
-  effort_limit=ACTUATOR_5020.effort_limit,
-  armature=ACTUATOR_5020.reflected_inertia,
-)
-G1_ACTUATOR_7520_14 = BuiltinPositionActuatorCfg(
-  joint_names_expr=(".*_hip_pitch_joint", ".*_hip_yaw_joint", "waist_yaw_joint"),
-  stiffness=STIFFNESS_7520_14,
-  damping=DAMPING_7520_14,
-  effort_limit=ACTUATOR_7520_14.effort_limit,
-  armature=ACTUATOR_7520_14.reflected_inertia,
-)
-G1_ACTUATOR_7520_22 = BuiltinPositionActuatorCfg(
-  joint_names_expr=(".*_hip_roll_joint", ".*_knee_joint"),
-  stiffness=STIFFNESS_7520_22,
-  damping=DAMPING_7520_22,
-  effort_limit=ACTUATOR_7520_22.effort_limit,
-  armature=ACTUATOR_7520_22.reflected_inertia,
-)
-G1_ACTUATOR_4010 = BuiltinPositionActuatorCfg(
-  joint_names_expr=(".*_wrist_pitch_joint", ".*_wrist_yaw_joint"),
-  stiffness=STIFFNESS_4010,
-  damping=DAMPING_4010,
-  effort_limit=ACTUATOR_4010.effort_limit,
-  armature=ACTUATOR_4010.reflected_inertia,
+  stiffness=_kp(ARM_ACTUATOR),
+  damping=_kv(ARM_ACTUATOR),
+  effort_limit=ARM_ACTUATOR.effort_limit,
+  armature=ARM_ACTUATOR.reflected_inertia,
 )
 
-# Waist pitch/roll and ankles are 4-bar linkages with 2 5020 actuators.
-# Due to the parallel linkage, the effective armature at the ankle and waist joints
-# is configuration dependent. Since the exact geometry of the linkage is unknown, we
-# assume a nominal 1:1 gear ratio. Under this assumption, the joint armature in the
-# nominal configuration is approximated as the sum of the 2 actuators' armatures.
-G1_ACTUATOR_WAIST = BuiltinPositionActuatorCfg(
-  joint_names_expr=("waist_pitch_joint", "waist_roll_joint"),
-  stiffness=STIFFNESS_5020 * 2,
-  damping=DAMPING_5020 * 2,
-  effort_limit=ACTUATOR_5020.effort_limit * 2,
-  armature=ACTUATOR_5020.reflected_inertia * 2,
+T1_ACTUATOR_WAIST_HIP_ROLL_YAW = BuiltinPositionActuatorCfg(
+  joint_names_expr=("Waist", ".*_Hip_Roll", ".*_Hip_Yaw"),
+  stiffness=_kp(WAIST_HIP_ROLL_YAW_ACTUATOR),
+  damping=_kv(WAIST_HIP_ROLL_YAW_ACTUATOR),
+  effort_limit=WAIST_HIP_ROLL_YAW_ACTUATOR.effort_limit,
+  armature=WAIST_HIP_ROLL_YAW_ACTUATOR.reflected_inertia,
 )
-G1_ACTUATOR_ANKLE = BuiltinPositionActuatorCfg(
-  joint_names_expr=(".*_ankle_pitch_joint", ".*_ankle_roll_joint"),
-  stiffness=STIFFNESS_5020 * 2,
-  damping=DAMPING_5020 * 2,
-  effort_limit=ACTUATOR_5020.effort_limit * 2,
-  armature=ACTUATOR_5020.reflected_inertia * 2,
+
+T1_ACTUATOR_HIP_PITCH = BuiltinPositionActuatorCfg(
+  joint_names_expr=(".*_Hip_Pitch",),
+  stiffness=_kp(HIP_PITCH_ACTUATOR),
+  damping=_kv(HIP_PITCH_ACTUATOR),
+  effort_limit=HIP_PITCH_ACTUATOR.effort_limit,
+  armature=HIP_PITCH_ACTUATOR.reflected_inertia,
+)
+
+T1_ACTUATOR_KNEE = BuiltinPositionActuatorCfg(
+  joint_names_expr=(".*_Knee_Pitch",),
+  stiffness=_kp(KNEE_ACTUATOR),
+  damping=_kv(KNEE_ACTUATOR),
+  effort_limit=KNEE_ACTUATOR.effort_limit,
+  armature=KNEE_ACTUATOR.reflected_inertia,
+)
+
+T1_ACTUATOR_ANKLE = BuiltinPositionActuatorCfg(
+  joint_names_expr=(".*_Ankle_Pitch", ".*_Ankle_Roll"),
+  stiffness=_kp(ANKLE_ACTUATOR),
+  damping=_kv(ANKLE_ACTUATOR),
+  effort_limit=ANKLE_ACTUATOR.effort_limit,
+  armature=ANKLE_ACTUATOR.reflected_inertia,
 )
 
 ##
-# Keyframe config.
+# Keyframes.
 ##
 
 HOME_KEYFRAME = EntityCfg.InitialStateCfg(
-  pos=(0, 0, 0.783675),
+  pos=(0.0, 0.0, 0.665),
   joint_pos={
-    ".*_hip_pitch_joint": -0.1,
-    ".*_knee_joint": 0.3,
-    ".*_ankle_pitch_joint": -0.2,
-    ".*_shoulder_pitch_joint": 0.2,
-    ".*_elbow_joint": 1.28,
-    "left_shoulder_roll_joint": 0.2,
-    "right_shoulder_roll_joint": -0.2,
-  },
-  joint_vel={".*": 0.0},
-)
-
-KNEES_BENT_KEYFRAME = EntityCfg.InitialStateCfg(
-  pos=(0, 0, 0.76),
-  joint_pos={
-    ".*_hip_pitch_joint": -0.312,
-    ".*_knee_joint": 0.669,
-    ".*_ankle_pitch_joint": -0.363,
-    ".*_elbow_joint": 0.6,
-    "left_shoulder_roll_joint": 0.2,
-    "left_shoulder_pitch_joint": 0.2,
-    "right_shoulder_roll_joint": -0.2,
-    "right_shoulder_pitch_joint": 0.2,
+    "Left_Shoulder_Roll": -1.4,
+    "Left_Elbow_Yaw": -0.4,
+    "Right_Shoulder_Roll": 1.4,
+    "Right_Elbow_Yaw": 0.4,
+    ".*_Hip_Pitch": -0.2,
+    ".*_Knee_Pitch": 0.4,
+    ".*_Ankle_Pitch": -0.2,
   },
   joint_vel={".*": 0.0},
 )
@@ -223,83 +150,58 @@ KNEES_BENT_KEYFRAME = EntityCfg.InitialStateCfg(
 # Collision config.
 ##
 
-# This enables all collisions, including self collisions.
-# Self-collisions are given condim=1 while foot collisions
-# are given condim=3.
+_foot_regex = r"^(left|right)_foot\d+_collision$"
+
 FULL_COLLISION = CollisionCfg(
   geom_names_expr=(".*_collision",),
-  condim={r"^(left|right)_foot[1-7]_collision$": 3, ".*_collision": 1},
-  priority={r"^(left|right)_foot[1-7]_collision$": 1},
-  friction={r"^(left|right)_foot[1-7]_collision$": (0.6,)},
-)
-
-FULL_COLLISION_WITHOUT_SELF = CollisionCfg(
-  geom_names_expr=(".*_collision",),
-  contype=0,
-  conaffinity=1,
-  condim={r"^(left|right)_foot[1-7]_collision$": 3, ".*_collision": 1},
-  priority={r"^(left|right)_foot[1-7]_collision$": 1},
-  friction={r"^(left|right)_foot[1-7]_collision$": (0.6,)},
-)
-
-# This disables all collisions except the feet.
-# Feet get condim=3, all other geoms are disabled.
-FEET_ONLY_COLLISION = CollisionCfg(
-  geom_names_expr=(r"^(left|right)_foot[1-7]_collision$",),
-  contype=0,
-  conaffinity=1,
-  condim=3,
+  solref=(0.01, 1),
+  condim={_foot_regex: 6, ".*_collision": 3},
+  friction={_foot_regex: (1, 5e-3, 5e-4), ".*_collision": (0.6,)},
   priority=1,
-  friction=(0.6,),
 )
 
 ##
 # Final config.
 ##
 
-G1_ARTICULATION = EntityArticulationInfoCfg(
+T1_ARTICULATION = EntityArticulationInfoCfg(
   actuators=(
-    G1_ACTUATOR_5020,
-    G1_ACTUATOR_7520_14,
-    G1_ACTUATOR_7520_22,
-    G1_ACTUATOR_4010,
-    G1_ACTUATOR_WAIST,
-    G1_ACTUATOR_ANKLE,
+    T1_ACTUATOR_NECK,
+    T1_ACTUATOR_ARM,
+    T1_ACTUATOR_WAIST_HIP_ROLL_YAW,
+    T1_ACTUATOR_HIP_PITCH,
+    T1_ACTUATOR_KNEE,
+    T1_ACTUATOR_ANKLE,
   ),
   soft_joint_pos_limit_factor=0.9,
 )
 
 
-def get_g1_robot_cfg() -> EntityCfg:
-  """Get a fresh G1 robot configuration instance.
-
-  Returns a new EntityCfg instance each time to avoid mutation issues when
-  the config is shared across multiple places.
-  """
+def get_t1_robot_cfg() -> EntityCfg:
+  """Get a fresh T1 robot configuration instance."""
   return EntityCfg(
-    init_state=KNEES_BENT_KEYFRAME,
+    init_state=HOME_KEYFRAME,
     collisions=(FULL_COLLISION,),
     spec_fn=get_spec,
-    articulation=G1_ARTICULATION,
+    articulation=T1_ARTICULATION,
   )
 
 
-G1_ACTION_SCALE: dict[str, float] = {}
-for a in G1_ARTICULATION.actuators:
+T1_ACTION_SCALE: dict[str, float] = {}
+for a in T1_ARTICULATION.actuators:
   assert isinstance(a, BuiltinPositionActuatorCfg)
   e = a.effort_limit
   s = a.stiffness
   names = a.joint_names_expr
   assert e is not None
   for n in names:
-    G1_ACTION_SCALE[n] = 0.25 * e / s
+    T1_ACTION_SCALE[n] = 0.25 * e / s
 
 
 if __name__ == "__main__":
   import mujoco.viewer as viewer
-
   from mjlab.entity.entity import Entity
 
-  robot = Entity(get_g1_robot_cfg())
+  robot = Entity(get_t1_robot_cfg())
 
   viewer.launch(robot.spec.compile())
