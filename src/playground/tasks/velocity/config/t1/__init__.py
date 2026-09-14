ae22ba74c3c2cbceedf2c65d8ce7e522635eeb86
Miguel Garcia <garcia.miguel.onate@gmail.com>
2026-09-14 13:18:25 -0600

    jeje

--- src/playground/tasks/velocity/config/t1/__init__.py
+++ src/playground/tasks/velocity/config/t1/__init__.py
@@ -3,7 +3,7 @@ from mjlab.tasks.velocity.rl import VelocityOnPolicyRunner
 
 from playground.rl.flashsac import VelocityOffPolicyRunner
 
-from .env_cfgs import t1_flat_env_cfg, t1_rough_env_cfg
+from .env_cfgs import t1_flat_env_cfg, t1_rough_env_cfg, t1_flat_env_cfg_flashsac, t1_rough_env_cfg_flashsac
 from .rl_cfg import t1_flashsac_runner_cfg, t1_ppo_runner_cfg
 
 register_mjlab_task(
@@ -24,16 +24,16 @@ register_mjlab_task(
 
 register_mjlab_task(
   task_id="Mjlab-Velocity-Rough-Booster-T1-FlashSAC",
-  env_cfg=t1_rough_env_cfg(),
-  play_env_cfg=t1_rough_env_cfg(play=True),
+  env_cfg=t1_rough_env_cfg_flashsac(),
+  play_env_cfg=t1_rough_env_cfg_flashsac(play=True),
   rl_cfg=t1_flashsac_runner_cfg(),
   runner_cls=VelocityOffPolicyRunner,
 )
 
 register_mjlab_task(
   task_id="Mjlab-Velocity-Flat-Booster-T1-FlashSAC",
-  env_cfg=t1_flat_env_cfg(),
-  play_env_cfg=t1_flat_env_cfg(play=True),
+  env_cfg=t1_flat_env_cfg_flashsac(),
+  play_env_cfg=t1_flat_env_cfg_flashsac(play=True),
   rl_cfg=t1_flashsac_runner_cfg(),
   runner_cls=VelocityOffPolicyRunner,
 )
--- src/playground/tasks/velocity/config/t1/env_cfgs.py
+++ src/playground/tasks/velocity/config/t1/env_cfgs.py
@@ -168,8 +168,6 @@ def t1_rough_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
     weight=-1.0,
     params={"sensor_name": self_collision_cfg.name, "force_threshold": 10.0},
   )
-  cfg.actions["joint_pos"].scale = 1.0
-
   # Apply play mode overrides.
   if play:
     # Effectively infinite episode length.
@@ -222,4 +220,18 @@ def t1_flat_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
     twist_cmd.ranges.lin_vel_x = (-1.0, 1.2)
     twist_cmd.ranges.ang_vel_z = (-0.7, 0.7)
 
+  return cfg
+
+def t1_flat_env_cfg_flashsac(play: bool = False) -> ManagerBasedRlEnvCfg:
+  """Create Booster T1 flat terrain velocity tracking configuration for FlashSAC."""
+  cfg = t1_flat_env_cfg(play=play)
+
+  cfg.actions["joint_pos"].scale = 1.0
+  return cfg
+
+def t1_rough_env_cfg_flashsac(play: bool = False) -> ManagerBasedRlEnvCfg:
+  """Create Booster T1 rough terrain velocity tracking configuration for FlashSAC."""
+  cfg = t1_rough_env_cfg(play=play)
+
+  cfg.actions["joint_pos"].scale = 1.0
   return cfg
