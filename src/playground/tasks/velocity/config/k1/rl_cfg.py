c2be37f9c0c205ba83bfc22d279a5e70eb80350b
Miguel Garcia <garcia.miguel.onate@gmail.com>
2026-09-22 00:03:07 -0600

    fixed flashsac k1 velocity training

--- src/playground/tasks/velocity/config/k1/rl_cfg.py
+++ src/playground/tasks/velocity/config/k1/rl_cfg.py
@@ -123,7 +123,7 @@ def k1_flashsac_runner_cfg(symmetry: bool = False) -> RslRlFlashSacRunnerCfg:
     ),
     experiment_name="k1_velocity_flashsac_da" if symmetry else "k1_velocity_flashsac",
     wandb_project="k1_velocity",
-    save_interval=1_000,
+    save_interval=4_000,
     num_steps_per_env=1,
-    max_iterations=6_000,
+    max_iterations=20_000,
   )
